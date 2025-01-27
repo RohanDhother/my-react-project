6b4b71328a9477b6cb7a5b1b80e44042b03a7533
rohandhother <rohandhother@gmail.com>
2025-01-27 19:38:40 +0000

    feat: change size of react icon depending where the mouse is on the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import reactLogo from './assets/react.svg'
 
 function App() {
   const [rotationDirection, setRotationDirection] = useState('forward');
+  const imageRef = useRef(null)
 
   const handleClick = () => {
     setRotationDirection(rotationDirection === 'forward' ? 'backward' : 'forward');
   };
+
+  useEffect(() => {
+      const handleMouseMove = (e) => {
+          const { clientX, clientY } = e;
+          const { innerWidth, innerHeight } = window;
+          const xRatio = clientX / innerWidth;
+          const yRatio = clientY / innerHeight;
+          const scale = 1 + (xRatio + yRatio) * 0.5;
+          imageRef.current.style.transform = `scale(${scale})`;
+      };
+
+      window.addEventListener('mousemove', handleMouseMove);
+
+      return () => {
+        window.removeEventListener('mousemove', handleMouseMove);
+      };
+  });
   return (
     <div>
-      <div>
-          <img
+      <div class='image-div'>
+          <div ref={imageRef}>
+            <img
             src={reactLogo}
             alt="React Logo"
             className={`react-icon rotating-${rotationDirection}`}
             onClick={handleClick}
           />
+          </div>
       </div>
     </div>
   );
